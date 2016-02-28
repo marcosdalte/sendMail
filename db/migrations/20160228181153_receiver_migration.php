28179da04ddeca16ecc2e7e5eee0bce2bf4e9e76
Marcos Dalte <marcosdalte@gmail.com>
2016-02-28 23:40:59 +0000

    migration table receiver

---
+++ db/migrations/20160228181153_receiver_migration.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ReceiverMigration extends AbstractMigration
+{
+    public function change()
+    {
+        // create the table receiver
+        $table = $this->table('receiver', array('id' => 'receiver_id'));
+        $table->addColumn('name', 'string', array('limit' => 45, 'null' => false))
+              ->addColumn('email', 'string', array('limit' => 45, 'null' => false))
+              ->addColumn('dt_birthday', 'date', array('limit' => 45, 'null' => false))
+              //Doesnt have support to enum
+              //->addColumn('bl_active', 'enum', array('limit' => array('y', 'n'), 'null' => false))
+              ->addColumn('bl_active', 'boolean', array('limit' => array('y', 'n'), 'null' => false))
+              ->addIndex(array('name', 'email'), array('unique' => true))
+              ->create();
+    }
+}
