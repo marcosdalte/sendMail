1e9dfa987ef82571ef1386daa9e72b1846001662
Marcos Dalte <marcosdalte@gmail.com>
2016-03-03 20:00:32 +0000

    add enum to column bl_active

--- db/migrations/20160228181153_receiver_migration.php
+++ db/migrations/20160228181153_receiver_migration.php
@@ -10,10 +10,8 @@ class ReceiverMigration extends AbstractMigration
         $table = $this->table('receiver', array('id' => 'receiver_id'));
         $table->addColumn('name', 'string', array('limit' => 45, 'null' => false))
               ->addColumn('email', 'string', array('limit' => 45, 'null' => false))
-              ->addColumn('dt_birthday', 'date', array('limit' => 45, 'null' => false))
-              //Doesnt have support to enum
-              //->addColumn('bl_active', 'enum', array('limit' => array('y', 'n'), 'null' => false))
-              ->addColumn('bl_active', 'string', array('limit' => 2, 'null' => false))
+              ->addColumn('dt_birthday', 'date', array('null' => false))
+              ->addColumn('bl_active', 'enum', array('values' => array('y', 'n'), 'null' => false))
               ->addIndex(array('name', 'email'), array('unique' => true))
               ->create();
     }
