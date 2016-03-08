31f205d75d63c216e5e5f5bb5630d7692df91b30
Marcos Dalte <marcosdalte@gmail.com>
2016-03-08 20:42:36 +0000

    rename table login to user

--- db/migrations/20160223233324_login_migration.php
+++ db/migrations/20160223233324_login_migration.php
@@ -7,7 +7,7 @@ class LoginMigration extends AbstractMigration
     public function change()
     {
         // create the table
-        $table = $this->table('login', array('id' => 'login_id'));
+        $table = $this->table('user', array('id' => 'user_id'));
         $table->addColumn('username', 'string', array('limit' => 45, 'null' => false))
               ->addColumn('password', 'string', array('limit' => 100, 'null' => false))
               ->addIndex('username', array('unique' => true))
