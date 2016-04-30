e2655b2b4f72e418a2da5e73e19a69999e11a489
Marcos Dalte <marcosdalte@gmail.com>
2016-04-30 21:37:37 +0000

    development module employee

--- db/migrations/20160427002605_add_contraint_employee.php
+++ db/migrations/20160427002605_add_contraint_employee.php
@@ -9,7 +9,6 @@ class AddContraintEmployee extends AbstractMigration
         $table = $this->table('employee');
         $table->addColumn('teste','string')
             ->addIndex(array('receiver_id'), array('unique' => true, 'name' => 'uk_receiver_id'))
-            //->addIndex('teste', array('unique' => true,  'name' => 'idx_users_email'))
             ->save();
     }
 }
