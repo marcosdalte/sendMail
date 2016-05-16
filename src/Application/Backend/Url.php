88b85c9a335f6b089d32ae0238bc21487153d535
Marcos Dalte <marcosdalte@gmail.com>
2016-05-16 00:31:08 +0000

    new url to api template

--- src/Application/Backend/Url.php
+++ src/Application/Backend/Url.php
@@ -12,9 +12,9 @@ namespace Application\Backend {
                 '/^\/api\/login\/auth\/?$/'       => ['Backend/Login/auth',['POST','GET']],
                 '/^\/api\/receivers(\/[0-9]+)?$/' => ['Backend/Receiver/dispatch', ['GET','POST','DELETE','PUT']],
                 '/^\/api\/users(\/[0-9]+)?$/' => ['Backend/User/dispatch', ['GET','POST','DELETE','PUT']],
-                '/^\/api\/employees(\/[0-9]+)?$/' => ['Backend/Employee/dispatch', ['GET','POST','DELETE','PUT']]];
+                '/^\/api\/employees(\/[0-9]+)?$/' => ['Backend/Employee/dispatch', ['GET','POST','DELETE','PUT']],
+                '/^\/api\/templates(\/[0-9]+)?$/' => ['Backend/Template/dispatch', ['GET','POST','DELETE','PUT']]];
                 //'/^\/api\/receivers(\/(?P<id>\d+)\/(?P<nome>\w+))?$/' => ['Backend/Receiver/dispatch', ['GET','POST','DELETE','PUT']],
-
             return $url;
         }
     }
