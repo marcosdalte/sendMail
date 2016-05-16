843d96c029e3907b7c7bce6a81f078633876949d
Marcos Dalte <marcosdalte@gmail.com>
2016-05-16 00:32:38 +0000

    script to test api template

---
+++ tests/addTemplate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -X POST -d "{\"path\": \"./templates/pascoa/pascoa.html\",\"cd_template\": \"PASCOA\",\"description\": \"Páscoa\",\"bl_active\": \"n\"}" https://sendmail-upd-marcosdalte.c9users.io/willer/api/templates
+echo
